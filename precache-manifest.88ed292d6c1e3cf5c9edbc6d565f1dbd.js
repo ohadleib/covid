d7f291a39461fb358f9c1ababc4b763966a31a73
אוהד ליבוביץ <ohad@whds-MacBook-Pro.local>
2020-10-14 21:52:40 +0300

    Updates

---
+++ precache-manifest.88ed292d6c1e3cf5c9edbc6d565f1dbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f49078fd5b261af046556d64f1c1a2",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "08fd6638f11f5df98a4c",
+    "url": "/covid-19/static/css/2.0c73dda4.chunk.css"
+  },
+  {
+    "revision": "a8257f2acf5aa925a680",
+    "url": "/covid-19/static/css/main.1aa03b75.chunk.css"
+  },
+  {
+    "revision": "08fd6638f11f5df98a4c",
+    "url": "/covid-19/static/js/2.33b002d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19/static/js/2.33b002d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8257f2acf5aa925a680",
+    "url": "/covid-19/static/js/main.e8ad3a26.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  }
+]);
